1dbba3ca5402886f394a53e219de1aa3414b8a5b
SANHNV <45290025+SANHNV@users.noreply.github.com>
2021-01-31 19:12:18 +0100

    error on calling displayOptions(js) from python

--- eel_wikigame.py
+++ eel_wikigame.py
@@ -10,10 +10,13 @@ import re
 
 #Get url and title from random wiki url
 def ReturnLink(link):
-    with urllib.request.urlopen(link) as response:
-        webpage = response.read()
-        soup = BeautifulSoup(webpage, 'html.parser')
-        return (response.url, soup.find(id="firstHeading").get_text())
+    try:
+        with urllib.request.urlopen(link) as response:
+            webpage = response.read()
+            soup = BeautifulSoup(webpage, 'html.parser')
+            return (response.url, soup.find(id="firstHeading").get_text())
+    except:
+        print("ERROR with ReturnLink at the random link selectio /!\\")
 
 #region Initialization
 
@@ -24,30 +27,70 @@ linkFinish = ReturnLink(linkRandom)
 likeBase = "https://fr.wikipedia.org"
 
 #Variables
-global linkActive
-listLinkToIgnore = ([])
+global linkActive 
+linkActive = linkStart
+listLinkToIgnore = [("","")]
+
 score = 0
 
-#GUI
+#Clean up the links captured
+def CheckLinkToKeep(link):
+    return link != None and not (link in listLinkToIgnore) and not (link.startswith('/w/index')) and not (link.startswith('http')) and not (link.startswith('#')) and not (link.startswith('/wiki/Cat%C3%A9gorie')) and not (link.startswith('/wiki/Portail:')) and not (link.startswith('/wiki/Fichier')) and not (link.startswith('/wiki/Aide')) and not (link.startswith('/wiki/Projet'))
+
+#Format tuple to string = "tuple0=tuple1"
+def TupleToJs(tuple):
+    return tuple[0] + "=" + tuple[1]
+
+#region GUI eel
+
+#start gui
 eel.init('web')
 
-#endregion
-
-#Show score first time on web
+#Show score on web
 @eel.expose
 def showScore():
     temp = score if score !=0 else "Let's get started"
-    eel.showScore_js(linkStart[1], linkStart[1], linkFinish[1], temp)
+    eel.showScore_js(linkStart[1], TupleToJs(linkActive), linkFinish[1], temp)
 
-#region Test eel
+#Filter list of links for next jump and return it
 @eel.expose
-def get_random_name():
-    eel.prompt_alerts('Random name')
+def getLinks(link):
+    listLink = [("","")]
+    listLink.remove(("",""))
 
-@eel.expose
-def get_random_number():
-    eel.prompt_alerts(random.randint(1, 100))
+    #Get all the link in the page
+    print(link)
+    with urllib.request.urlopen(link) as response:
+        webpage = response.read()
+        soup = BeautifulSoup(webpage, 'html.parser')
+        for anchor in soup.find_all(id="content"):
+
+            #Get all the links in main content
+            for a in anchor.findChildren("a") :
+                if(a.get('href') != None) :
+                    listLink.append((a.get('href'), a.get_text()))
+
+            #Get links to ignore summary and panels
+            for summary in anchor.find_all(id="toc"):
+                for a in summary.findChildren("a") :
+                    listLinkToIgnore.append(a.get('href'))
+            for rightSide in anchor.find_all('div', {'class': re.compile(r"^infobox")}):
+                for a in rightSide.findChildren("a") :
+                    listLinkToIgnore.append(a.get('href'))
+
+        #Clean up the list
+        listLink[:] = [(x,_) for (x,_) in listLink if CheckLinkToKeep(x)]
+        listjs = []
+        for i in listLink:
+            listjs.append(TupleToJs(i))
+        
+        print("Jusqu'ici, tout va bien.")
+        print(listjs)
+        eel.displayOptions(listLink)
+
 #endregion
 
 #start GUI
 eel.start('index.html')
+
+#endregion
--- web/js/main.js
+++ web/js/main.js
@@ -1,30 +1,60 @@
 color = ["bg-primary", "bg-success", "bg-info", "bg-warning", "bg-danger", "bg-secondary", "bg-dark", "bg-light"];
+linkActive_js = ["", ""]
 
 //Update score and links
 eel.expose(showScore_js);
 function showScore_js(linkStart, linkActive, linkFinish, score){
+  linkActive_js = linkActive.split("=");
   document.getElementById("score").innerHTML = score;
   document.getElementById("startLink").innerHTML = linkStart;
-  document.getElementById("activeLink").innerHTML = linkActive;
+  document.getElementById("activeLink").innerHTML = linkActive_js[1];
   document.getElementById("finishLink").innerHTML = linkFinish;
+  document.getElementById("options").innerHTML = typeof(linkActive);
+  eel.getLinks(linkActive_js[0]);
+}
+
+eel.expose(displayOptions);
+function displayOptions(links){
+  // var list = []
+  // links.forEach(element => {
+  //   list.push(element.slice(element.indexOf("=")+1, element.lenght));
+  // });
+  
+  var parent = document.getElementById("options");
+  links.forEach(element => {
+    var child = document.createAttribute("li");
+    child.className = "list-group-item " + color[Math.floor(Math.random() * 8)];
+    child.setAttribute("id", element.slice(0, element.indexOf("=")));
+    child.innerText = element.slice(element.indexOf("=")+1, element.lenght);
+    parent.appendChild(child);
+  });
 }
 
 //Call when the game start the first time
 function gameStart(){
   document.getElementById("game").style.display = "block";
-  document.getElementById("startGame").setAttribute("hidden", "none");
+  document.getElementById("button_startGame").setAttribute("hidden", "none");
+  //as the score to python
   eel.showScore()
 }
 
-//button start game
-document.getElementById("startGame").addEventListener("click", gameStart);
+//Show Rules
+document.getElementById("button_rules").addEventListener("click", ()=>
+{
+  var element = document.getElementById("rules");
+  if (element.style.display == "none")
+  {
+    element.style.display = "block";
+    document.getElementById("button_rules").innerText = "Hide Rules"
+  }
+  else{
+    element.style.display = "none";
+    document.getElementById("button_rules").innerText = "Show Rules"
+  }
+});
 
-//#region Test eel
-eel.expose(prompt_alerts);
-function prompt_alerts(description) {
-  alert(description);
-}
+//Button start game or restart 
+document.getElementById("button_startGame").addEventListener("click", gameStart);
+document.getElementById("button_restart").addEventListener("click", gameStart);
 
-document.getElementById("button-name").addEventListener("click", ()=>{eel.start()}, false);
-document.getElementById("button-number").addEventListener("click", ()=>{eel.get_random_number()}, false);
 //#endregion
